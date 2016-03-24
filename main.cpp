0476ba489113ec0de1cc8c5569bfd4e40e531fcb
Storm <842222375@qq.com>
2016-03-24 18:13:45 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void main() {
+	auto start = clock();
+	tmain();
+	auto end = clock();
+	cout << "Elapsed time : " << end - start << " ms!" << endl;
+	system("PAUSE");
+}
+
+void tmain() {
+
+}
