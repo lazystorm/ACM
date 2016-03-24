58c63992c2d1d07f1e7aba6acc36f50fa66c96c5
Storm <842222375@qq.com>
2016-03-24 18:14:54 +0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cctype>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <ctime>
+#include <functional>
+#include <array>
+#include <stdio.h>
+#include <bitset>
+#include <xmmintrin.h>
+#include <immintrin.h>
+#include <cstdint>
+#include <cstdlib>
+#include <numeric>
+
+using namespace std;
